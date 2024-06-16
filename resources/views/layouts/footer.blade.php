fbb0ece72d136b86e2526edb974c5d578902f889
MpxMpx <toankim12354@gmail.com>
2024-06-16 20:15:00 +0700

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,70 @@
+<footer class="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-4 col-xs-6 col-ssm">
+          <div class="footer-widget">
+            <div class="widget-title">
+              <h3>{{ __('footer.AboutUs') }}</h3>
+            </div>
+            <div class="widget-content">
+              <p><b><i class="fas fa-mobile-alt"></i> Hotline:</b> <a href="tel:(+84) 343754517">(+84) 343754517</a></p>
+              <p><b><i class="fas fa-envelope"></i> Email:</b> <a href="mailto:thanhtrung@gmail.com" rel="nofollow">thanhtrung@gmail.com</a></p>
+              <p><b><i class="fas fa-store-alt"></i> Địa chỉ:</b> Phường 25 - Bình Thạnh - Hồ Chí Minh</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 hidden-sm hidden-xs">
+          <div class="footer-widget">
+            <div class="widget-title">
+              <h3>{{ __('footer.support') }}</h3>
+            </div>
+            <div class="widget-content">
+              <ul>
+                <li><a href="#" title="Chính sách bảo hành">Chính sách bảo hành</a></li>
+                <li><a href="#" title="Chính sách vận chuyển">Chính sách vận chuyển</a></li>
+                <li><a href="#" title="Chính sách đổi trả hàng">Chính sách đổi trả hàng</a></li>
+                <li><a href="#" title="Hướng dẫn thanh toán">Hướng dẫn thanh toán</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 col-ssm">
+          <div class="footer-widget">
+            <div class="widget-title">
+              <h3>{{ __('footer.payment') }}</h3>
+            </div>
+            <div class="widget-content">
+              <ul>
+                <li class="clearfix">
+                  <div class="col-md-3 col-sm-3 col-xs-3 padding-lr5" title="{{ __('footer.visa') }}"><div class="payment visa"><img src="{{ asset('images/visa.png') }}"></div></div>
+                  <div class="col-md-3 col-sm-3 col-xs-3 padding-lr5" title="{{ __('footer.mastercard') }}"><div class="payment"><img src="{{ asset('images/mastercard.png') }}"></div></div>
+                  <div class="col-md-3 col-sm-3 col-xs-3 padding-lr5" title="{{ __('footer.jcb') }}"><div class="payment"><img src="{{ asset('images/jcb.png') }}"></div></div>
+                  <div class="col-md-3 col-sm-3 col-xs-3 padding-lr5" title="{{ __('footer.cod') }}"><div class="payment"><img src="{{ asset('images/cod.png') }}"></div></div>
+                </li>
+                <li>
+                  <div class="payment-service" title="{{ __('footer.nganluong') }}"><img src="{{ asset('images/nganluong.png') }}"></div>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 hidden-xs">
+          <div class="footer-widget">
+            <div class="widget-title">
+              <!-- <h3>{{ __('footer.FollowUs') }}</h3> -->
+              <h3>Địa Chỉ Google Map</h3>
+            </div>
+            <div class="widget-content">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.5177580567047!2d106.69916291428699!3d10.771600262229411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f40a3b49e59%3A0xa1bd14e483a602db!2sCao%20Thang%20Technical%20College!5e0!3m2!1sen!2s!4v1673944540103!5m2!1sen!2s" width="270" height="130" style="border: none; overflow: hidden; max-width: 100%;" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="copyright text-center">
+      Copyright &copy 2023 Nguyễn Thành Trung. All rights reserved.
+    </div>
+  </div>
+</footer>
